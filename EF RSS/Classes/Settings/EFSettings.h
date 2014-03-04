1ef9f84947a9d5ea8e12b695b8a776ff3cdb6806
Bruno Koga <koga@brunokoga.com>
2014-03-04 11:52:13 -0300

    Adds settings to change the URL

---
+++ EF RSS/Classes/Settings/EFSettings.h
@@ -0,0 +1,27 @@
+//
+//  EFSettings.h
+//  EF RSS
+//
+//  Created by Bruno Koga on 3/4/14.
+//  Copyright (c) 2014 Bruno Koga. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface EFSettings : NSObject
+
+
+//TODO: write unit tests
+
+/*
+ Returns the stored feed url string.
+ If there isn't a stored feed url string, will return the default one.
+ */
+
++ (NSString *)feedURLString;
+
+/*
+ Stores the feed url string
+ */
++ (void)setFeedURLString:(NSString *)feedURLString;
+@end
---
+++ EF RSS/Classes/Settings/EFSettings.m
@@ -0,0 +1,30 @@
+//
+//  EFSettings.m
+//  EF RSS
+//
+//  Created by Bruno Koga on 3/4/14.
+//  Copyright (c) 2014 Bruno Koga. All rights reserved.
+//
+
+#import "EFSettings.h"
+
+@implementation EFSettings
+
+static NSString * const kSettingsFeedURLStringKey = @"kSettingsFeedURLStringKey";
+static NSString * const kFeedDefaultURLString = @"http://feeds.bbci.co.uk/news/rss.xml";
+
++ (NSString *)feedURLString
+{
+  NSString *feedURLString;
+  feedURLString = [[NSUserDefaults standardUserDefaults] stringForKey:kSettingsFeedURLStringKey];
+  if ([feedURLString length] == 0) {
+    feedURLString = kFeedDefaultURLString;
+  }
+  return feedURLString;
+}
+
++ (void)setFeedURLString:(NSString *)feedURLString
+{
+  [[NSUserDefaults standardUserDefaults] setObject:feedURLString forKey:kSettingsFeedURLStringKey];
+}
+@end
