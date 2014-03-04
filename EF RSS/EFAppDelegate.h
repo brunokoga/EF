ee0a2590eb236880d720df54ba370bb605afafa0
Bruno Koga <koga@brunokoga.com>
2014-03-04 11:42:23 -0300

    Creates empty Project

---
+++ EF RSS/EFAppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  EFAppDelegate.h
+//  EF RSS
+//
+//  Created by Bruno Koga on 3/4/14.
+//  Copyright (c) 2014 Bruno Koga. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EFAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ EF RSS/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  EF RSS
+//
+//  Created by Bruno Koga on 3/4/14.
+//  Copyright (c) 2014 Bruno Koga. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "EFAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([EFAppDelegate class]));
+  }
+}
